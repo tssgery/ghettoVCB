515823bad06091a30e6bc7b082642be28776f891
Eric Young <eric@aceshome.com>
2017-03-03 13:15:08 -0500

    moving backups to different datastore

--- ghettoVCB.sh
+++ ghettoVCB.sh
@@ -12,7 +12,7 @@ LAST_MODIFIED_DATE=2020_12_28
 VERSION=1
 
 # directory that all VM backups should go (e.g. /vmfs/volumes/SAN_LUN1/mybackupdir)
-VM_BACKUP_VOLUME=/vmfs/volumes/vmbackup/ghetto
+VM_BACKUP_VOLUME=/vmfs/volumes/backup/ghetto
 
 # Format output of VMDK backup
 # zeroedthick
